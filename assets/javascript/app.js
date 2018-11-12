704b9288c428494a7cfda26a348b209ca8fd3b2a
Benny V <bengv2303@gmail.com>
2018-11-11 18:08:17 -0700

    HW updates

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+<script type="text/javascript">
+// Example queryURL for Giphy API
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
+</script>
